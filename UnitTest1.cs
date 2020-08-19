d874370905d728505fed70263583497b6a439247
sikarin <so.sikarin@perfenterprise.com>
2020-08-19 10:55:00 +0700

    add test case

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,14 +1,19 @@
 using System;
 using Xunit;
+using Homework;
 
 namespace test002
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(3, 4, 7)]
+        [InlineData(5, 6, 11)]
+        public void Test1(int num1, int num2, int num3)
         {
-            System.Console.WriteLine("Hello!!");
+            var sut = new HomeWork01();
+            var result = sut.Push(num1, num2);
+            Assert.Equal(num3, result);
         }
     }
 }
