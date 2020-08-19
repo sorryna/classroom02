b99ee364d271c65e848fe8b5c60f5c14db799214
sikarin <so.sikarin@perfenterprise.com>
2020-08-19 10:07:31 +0700

    Hellogit add .

---
+++ UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace test002
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            System.Console.WriteLine("Hello!!");
+        }
+    }
+}
